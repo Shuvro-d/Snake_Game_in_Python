28f29d74f8ef875b5d84b00dc402f03574c600b2
Shuvro Das <70909095+Shuvro-d@users.noreply.github.com>
2023-06-09 10:52:16 +0600

    SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -12,21 +12,28 @@ SIZE = 20
 MAX_VALUE=CANVAS_WIDTH-SIZE
 SNAKE_LENGTH = 20
 
+LEVEL_UP_SCORE=50
+MAX_LEVEL=5
+
 START_X = 0
 START_Y = 0
 #                                  Remaining Bugs
-#There are a lot of bugs. I solved as much as I can and the rests are beyond my capabilities
+#There are a lot of corner case. I solved as much as I can and the rests are beyond my capabilities
 #right now. One of them is when you collide in a wall and you have life you randomly appear in the canvas.
 #I made sure the snake doesn't appear just near the wall and game gets over by colliding to wall, but I
 # am unable to figure out how to make sure the snake doesn't appear in front of an obstacle. I guess it
-#will depend on players luck for now.
+#will depend on players luck for now. ANd for level up the difficulty will be the life will appear less
+#and less later with each level. I did not increased the speed with level because it hurt my eyes. But anyone
+#wants to do it, knock your self out. Finally, there is only 5 levels because I think the game gets boring
+#with too many levels.
  
 # if you make this larger, the game will go slower
 DELAY = 0.1 
                  
-def create_goal_and_obstacle(canvas,score): # the function creates the goal, life and obstacle
+def create_goal_and_obstacle(canvas,score,level): # the function creates the goal, life and obstacle
+    const=level*3
     goal=None  #None so that when goal is not created it returns None
-    if score==0 or score%15!=0:
+    if score==0 or score%const!=0:
         goal_x=random.randint(0, MAX_VALUE)
         goal_y=random.randint(0, MAX_VALUE)
         goal=canvas.create_rectangle(goal_x,goal_y,goal_x+SIZE,goal_y+SIZE,'red')
@@ -54,7 +61,7 @@ def create_goal_and_obstacle(canvas,score): # the function creates the goal, lif
             check=canvas.find_overlapping(obs_x,obs_y,obs_x+SIZE,obs_y+SIZE)
          
     life=None   # life is none because it only appears if score is divisible by 15
-    if score>0 and score%15==0:
+    if score>0 and score%const==0:
         life_x=random.randint(0, MAX_VALUE)
         life_y=random.randint(0, MAX_VALUE)
         life=canvas.create_oval(life_x,life_y,life_x+SIZE,life_y+SIZE,'lime')
@@ -72,24 +79,76 @@ def create_goal_and_obstacle(canvas,score): # the function creates the goal, lif
 
 
 
-def game_over(canvas,score):  #Deletes all the object in the canvas and print score in a black background
-    #first delete all the obstacles, snake and goal from the canvas
-    overlapped_obj=canvas.find_overlapping(0,0, 
-                           CANVAS_WIDTH,CANVAS_HEIGHT)
-    for i in overlapped_obj: #deleting all the objects including snake, goal and obstacles
-        canvas.delete(i)
+def game_over(canvas,score,level):  #Deletes all the object in the canvas and print score in a black background
+    
+    #creating the curtain to show at the end of the game
+    des_x=-CANVAS_WIDTH
+    des_y=-CANVAS_HEIGHT
+    des_width=CANVAS_HEIGHT
+    des=canvas.create_rectangle(des_x,des_y,des_x+des_width,des_y+des_width+500,'black')
+    canvas.create_text(50, 170,
+        "***Game Over***", 
+        color="white", 
+        font="Courier", 
+        font_size=35)
+    if level==MAX_LEVEL:
+        canvas.create_text(60,100 , 
+        "You reached MAX level, Hurrah! ",
+        color="white", 
+        font="Courier", 
+        font_size=20)
+    else:
+        canvas.create_text(60,100 , 
+        "You reached level ",
+        color="white", 
+        font="Courier", 
+        font_size=30)
+        canvas.create_text(380, 100, 
+        str(level),
+        color="white", 
+        font="Courier", 
+        font_size=30)
+    #shows game over animation
+    while des_x<0:
+        des_x+=5
+        overlapped_obj=canvas.find_overlapping(des_x,des_y, 
+                          des_x+des_width,des_y+des_width+500)
+        
+        for i in overlapped_obj: #deleting all the objects including snake, goal and obstacles
+            if i!=des:
+                canvas.delete(i)
+        canvas.moveto(des, des_x, des_y)
+        time.sleep(0.001) 
+        pass
         
     #create the background
     background = canvas.create_rectangle(0, 0,CANVAS_WIDTH,CANVAS_HEIGHT,
                               'black')
     #then print the score
+    if level==MAX_LEVEL:
+        canvas.create_text(60,100 , 
+        "You reached MAX level, Hurrah! ",
+        color="white", 
+        font="Courier", 
+        font_size=20)
+    else:
+        canvas.create_text(60,100 , 
+        "You reached level ",
+        color="white", 
+        font="Courier", 
+        font_size=30)
+        canvas.create_text(380, 100, 
+        str(level),
+        color="white", 
+        font="Courier", 
+        font_size=30)
     canvas.create_text(50, 170,
         "***Game Over***", 
         color="white", 
         font="Courier", 
         font_size=35)
     canvas.create_text(60,200 , 
-        "***Score is =     ***",
+        "***Score is =       ***",
         color="white", 
         font="Courier", 
         font_size=30)
@@ -103,14 +162,14 @@ def game_over(canvas,score):  #Deletes all the object in the canvas and print sc
     
 # this functions prints score and power in the left lower corner, when player is playing the game    
 def print_score(canvas,score):
-    text=canvas.create_text(145, PRINT_X, 
+    text=canvas.create_text(110, PRINT_X, 
         str(score),
         color="black", 
         font="Courier", 
         font_size=20)
     return text
 def print_power(canvas,x):
-    power=canvas.create_text(145, PRINT_Y, 
+    power=canvas.create_text(110, PRINT_Y, 
         str(x),
         color="black", 
         font="Courier", 
@@ -120,13 +179,57 @@ def print_power(canvas,x):
     
 
 # this is the most important function where all the codes for playing the game
-def start_game(canvas):
+def start_game(canvas,score,level):
+    #starting animation
+    des_x=-CANVAS_WIDTH
+    des_y=-CANVAS_HEIGHT
+    des_width=CANVAS_HEIGHT
+    des=canvas.create_rectangle(des_x,des_y,des_x+des_width,des_y+des_width+500,'purple')
+    if level==1:
+        text=canvas.create_text(50, 170,
+        "Game starting!!!", 
+        color="white", 
+        font="Courier", 
+        font_size=35)
+        textx=canvas.create_text(70, 210,
+        "At level     !!!", 
+        color="white", 
+        font="Courier", 
+        font_size=32)
+        textl=canvas.create_text(240, 210,
+        str(level), 
+        color="white", 
+        font="Courier", 
+        font_size=32)
+    else:
+        text=canvas.create_text(150, 170,
+        "Hurrah !!!", 
+        color="white", 
+        font="Courier", 
+        font_size=35)
+        textx=canvas.create_text(70, 210,
+        "Moving to level   !!!", 
+        color="white", 
+        font="Courier", 
+        font_size=32)
+        textl=canvas.create_text(370, 210,
+        str(level), 
+        color="white", 
+        font="Courier", 
+        font_size=32)
+    #shows game loading animation
+    while des_x<0:
+        des_x+=5
+        canvas.moveto(des, des_x, des_y)
+        time.sleep(0.001) 
+    canvas.delete(des)
+    canvas.delete(text)
     INITIAL_VELOCITY = 12
     x_velocity = INITIAL_VELOCITY
     y_velocity = 0
     snake_x = START_X
     snake_y = START_Y
-    score=0    #score and power is initially zero
+    #score and power is initially zero
     power=0
     
 
@@ -137,16 +240,16 @@ def start_game(canvas):
                               'blue')
                               
     #calling the goal function to create the first goal
-    goal,obstacle,life=create_goal_and_obstacle(canvas,score)
+    goal,obstacle,life=create_goal_and_obstacle(canvas,score,level)
     
     #create the score and power initializer at the lower left corner
-    canvas.create_text(10,PRINT_X , 
-        "Score is =  ",
+    a=canvas.create_text(10,PRINT_X , 
+        "Score =  ",
         color="black", 
         font="Courier", 
         font_size=20)
-    canvas.create_text(10,PRINT_Y , 
-        "Power is =  ",
+    b=canvas.create_text(10,PRINT_Y , 
+        "Power =  ",
         color="black", 
         font="Courier", 
         font_size=20)
@@ -193,7 +296,7 @@ def start_game(canvas):
                               snake_x + SNAKE_LENGTH,
                               snake_y + SNAKE_LENGTH)
             else:   #snake dies
-                game_over(canvas,score)
+                game_over(canvas,score,level)
                 break
             pass
         elif (snake_y < 0) or (snake_y + SNAKE_LENGTH >= CANVAS_HEIGHT): # snake is colliding with wall
@@ -227,11 +330,11 @@ def start_game(canvas):
                               snake_x + SNAKE_LENGTH,
                               snake_y + SNAKE_LENGTH)
             else:  #snake dies
-                game_over(canvas,score)
+                game_over(canvas,score,level)
                 break
             pass
         if flag2==False:   # this make sure after reviving the snake doesn't take goal as obstacle. I put this because I am 
-            goal,obstacle,life=create_goal_and_obstacle(canvas,score)# unable to figure out how to solve this issue
+            goal,obstacle,life=create_goal_and_obstacle(canvas,score,level)# unable to figure out how to solve this issue
         
         
         
@@ -293,24 +396,39 @@ def start_game(canvas):
                         if i!=snake:
                             canvas.delete(i)
                 else:   #snake dies 
-                    game_over(canvas,score)
+                    game_over(canvas,score,level)
                     break
             if score%15==0 and score>0:
                 INITIAL_VELOCITY+=1   #increas speed after every 15 points
             if flag==True:
-                goal,obstacle,life=create_goal_and_obstacle(canvas,score)  #create new goal after snake eats one
+                goal,obstacle,life=create_goal_and_obstacle(canvas,score,level)  #create new goal after snake eats one
             
-      
+        
+        
+        if score==LEVEL_UP_SCORE and level!=MAX_LEVEL:
+            canvas.delete(a)
+            canvas.delete(b)
+            canvas.delete(text)
+            canvas.delete(px)
+            o=canvas.find_overlapping(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
+            for i in o:
+                canvas.delete(i)
+            #print("Breaking the loop")
+            break
+        
         snake_x += x_velocity
         snake_y += y_velocity
         canvas.moveto(snake, snake_x, snake_y)
         time.sleep(DELAY)
         pass
+    #print("Returning the score")
+    return score
 
 def main():
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
     # TODO: your code here
-    
+    score=0
+    level=1
     #creates a background
     background=canvas.create_rectangle(0, 0,
                               CANVAS_WIDTH,
@@ -368,7 +486,11 @@ def main():
             canvas.delete(start)
             canvas.delete(background)
             #starting the game
-            start_game(canvas)
+            score = start_game(canvas,score,level)
+            while score==LEVEL_UP_SCORE:#increasing the level
+                level+=1
+                if level<=MAX_LEVEL:
+                    score=start_game(canvas,0,level)
             break
     pass
         
